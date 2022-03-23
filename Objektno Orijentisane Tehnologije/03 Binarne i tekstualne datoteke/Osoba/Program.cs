7afdeaf7212b686c20ff1a0801230f20d5de5ee6
Danijel Jovanović <danijel.xda@gmail.com>
2022-03-23 16:06:55 +0100

    OOT v03

---
+++ Objektno Orijentisane Tehnologije/03 Binarne i tekstualne datoteke/Osoba/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Osoba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba o1 = new Osoba("Danijel", "Jovanovic");
+            Osoba o2 = new Osoba("Milena", "Kovacevic");
+            Osoba o3 = new Osoba(" ", " ");
+
+            List<Osoba> osobe = new List<Osoba>();
+            List<Osoba> osobe_read = new List<Osoba>();
+            osobe.Add(o1); osobe.Add(o2); osobe.Add(o3);
+            osobe.Add(o1); osobe.Add(o2); osobe.Add(o3);
+
+            Console.Write(o1);
+            Console.Write(o2);
+
+            snimiOsobu(o1);
+            snimiOsobu(o2);
+
+            ucitajOsobu(o3);
+            Console.WriteLine("\nUcitana osoba\n" + o3);
+
+            snimiOsobe(osobe);
+            ucitajOsobe(osobe_read);
+
+            Console.WriteLine("\nOsobe iz liste txt: ");
+            foreach(Osoba o in osobe_read)
+            {
+                Console.Write(o);
+            }
+        }
+        static void snimiOsobu(Osoba o)
+        {
+            using (BinaryWriter binWriter = new BinaryWriter(File.Open("osoba.dat", FileMode.Append)))
+            {
+                binWriter.Write(o.Ime);
+                binWriter.Write(o.Prezime);
+                binWriter.Write("\n");
+
+                binWriter.Close();
+            }
+        } 
+
+        static void ucitajOsobu(Osoba o)
+        {
+            using (BinaryReader binReader = new BinaryReader(File.Open("osoba.dat", FileMode.Open)))
+            {
+                string ime = binReader.ReadString();
+                string prezime = binReader.ReadString();
+
+                o.Ime = ime;
+                o.Prezime = prezime;
+
+                binReader.Close();
+            }
+        }
+
+        static void snimiOsobe(List<Osoba> osobe)
+        {
+            using (TextWriter txtWriter = new StreamWriter(File.Open("osobe.txt", FileMode.Append)))
+            {
+                foreach(Osoba o in osobe)
+                {
+                    txtWriter.WriteLine(o.Ime + " " + o.Prezime);
+                }
+
+                txtWriter.Close();
+            }
+        }
+
+        static void ucitajOsobe(List<Osoba> osobe)
+        {
+            using (TextReader txtReader = new StreamReader(File.Open("osobe.txt", FileMode.Open)))
+            {
+                if(File.Exists("osobe.txt"))
+                {
+                    while (true)
+                    {
+                        string str = txtReader.ReadLine();
+                        if(str == null)
+                        {
+                            break;
+                        }
+
+                        string[] splitovano = str.Split(' ');
+                        Osoba tmp = new Osoba(splitovano[0], splitovano[1]);
+                        osobe.Add(tmp);
+                    }
+
+                    txtReader.Close();
+                }
+                else
+                {
+                    Console.WriteLine("\nDatoteka ne postoji!");
+                }
+            }
+        }
+    }
+}
---
+++ Objektno Orijentisane Tehnologije/03 Binarne i tekstualne datoteke/Restoran/Restoran.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Restoran
+{
+    class Restoran
+    {
+        private string naziv;
+        private string adresa;
+        private List<Stavka> jelovnik;
+
+        public Restoran(string naziv, string adresa)
+        {
+            this.Naziv = naziv;
+            this.Adresa = adresa;
+            jelovnik = new List<Stavka>();
+        }
+
+        public string Naziv { get => naziv; set => naziv = value; }
+        public string Adresa { get => adresa; set => adresa = value; }
+        public List<Stavka> Stavke { get => jelovnik; set => jelovnik = value; }
+
+        public override string ToString()
+        {
+            string str = "Restoran \"" + naziv + "\"\nAdresa: " + adresa + "\n";
+            str += "\nJelovnik\n***********************\n";
+            foreach(Stavka s in jelovnik)
+            {
+                str += s.Naziv + " " + s.Cena + "\n";
+            }
+            str += "***********************\n";
+
+            return str;
+        }
+
+        public void Import(string putanja)
+        {
+            using (TextReader txtReader = new StreamReader(File.Open(putanja, FileMode.Open)))
+            {
+                if (File.Exists(putanja))
+                {
+                    while (true)
+                    {
+                        string str = txtReader.ReadLine();
+                        if (str == null)
+                        {
+                            break;
+                        }
+
+                        string[] splitovano = str.Split('|');                        
+                        foreach(Stavka s in jelovnik)
+                        {
+                            if(s.Naziv.Equals(splitovano[0]))
+                            {
+                                break; // vec postoji ne dodajemo isti naziv
+                            }    
+                        }
+                        Stavka tmp = new Stavka(splitovano[0], double.Parse(splitovano[1]));
+                        jelovnik.Add(tmp);
+                    }
+
+                    txtReader.Close();
+                }
+                else
+                {
+                    Console.WriteLine("\nDatoteka ne postoji!");
+                }
+            }
+        }
+
+        public void Sortiraj()
+        {
+            jelovnik.Sort((s1, s2) => s1.Cena.CompareTo(s2.Cena));
+            jelovnik.Reverse();
+        }
+
+        public void Export(string putanja)
+        {
+            using (TextWriter txtWriter = new StreamWriter(File.Open(putanja, FileMode.Create)))
+            {
+                txtWriter.WriteLine("Restoran \"" + naziv + "\"");
+                txtWriter.WriteLine("Adresa: " + adresa);
+                txtWriter.WriteLine("Jelovnik\n");
+                txtWriter.WriteLine("******************");
+
+                foreach (Stavka s in jelovnik)
+                {
+                    txtWriter.WriteLine(s.Naziv + " " + s.Cena);
+                }
+                txtWriter.WriteLine("******************");
+
+                txtWriter.Close();
+            }
+        }
+    }
+}
