304325892378d1318af80df646e90bb7c89f9236
Danijel Jovanović <danijel.xda@gmail.com>
2022-03-26 09:14:14 +0100

    v04 OOT

---
+++ Objektno Orijentisane Tehnologije/04 Priprema K1/Evidencija/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Evidencija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zaposleni z1 = new Zaposleni(14, "/", "/", 100, 10, 5);
+            Console.WriteLine(z1);
+
+            EvidencijaZaposlenih evidencijaZaposlenih = new EvidencijaZaposlenih("FTN", "Trg Dositeja Obradovica 6");
+            evidencijaZaposlenih.UcitajZaposlene("Zaposleni.txt");
+            Console.WriteLine(evidencijaZaposlenih);
+
+            Zaposleni best = evidencijaZaposlenih.NajboljiRadnik();
+            Console.WriteLine("Radnik sa najvecom platom: " + best.Id + ". " + best.Ime + " " + best.Prezime + " " + evidencijaZaposlenih.Plata(best.Id) + " RSD");
+        }
+    }
+}
