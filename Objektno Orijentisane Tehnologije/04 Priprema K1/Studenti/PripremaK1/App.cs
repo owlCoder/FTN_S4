3bab180d1bc9ac5ee453be031df7322fc7c2a2f0
Danijel Jovanović <danijel.xda@gmail.com>
2022-03-29 23:11:14 +0200

    v04 OOT

---
+++ Objektno Orijentisane Tehnologije/04 Priprema K1/Studenti/PripremaK1/App.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace PripremaK1
+{
+    class App
+    {
+        public static string LoadFromFile(string putanja)
+        {
+            string sadrzaj = "";
+
+            using (TextReader txt = new StreamReader(File.Open(putanja, FileMode.Open)))
+            {
+                while(true)
+                {
+                    string red = txt.ReadLine();
+
+                    if(red == null)
+                    {
+                        break;
+                    }
+
+                    sadrzaj += red;
+                }
+                txt.Close();
+            }
+            return sadrzaj;
+        }
+
+        public static void SaveToFile(string zaUpis, string putanja)
+        {
+            using(TextWriter tw = new StreamWriter(File.Open(putanja, FileMode.Create)))
+            {
+                tw.WriteLine(zaUpis);
+                tw.Close();
+            }
+        }
+    }
+}
