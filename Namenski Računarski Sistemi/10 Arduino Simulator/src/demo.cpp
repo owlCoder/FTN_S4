d4d4fe746cbb43b0b781531a6060d9ceeea55f62
Danijel Jovanović <danijel.xda@gmail.com>
2022-04-30 19:56:29 +0200

    v10 ARD_SIM

---
+++ Namenski Računarski Sistemi/10 Arduino Simulator/src/demo.cpp
@@ -0,0 +1,22 @@
+#include<arduinoPlatform.h>
+#include<tasks.h>
+#include<interrupts.h>
+#include<stdio.h>
+#include<serial.h>
+
+extern serial Serial;
+
+/*
+*/
+
+void setup()
+{
+    Serial.begin(9600);
+
+
+}
+
+void loop()
+{
+
+}
