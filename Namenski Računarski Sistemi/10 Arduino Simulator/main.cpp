fc8fba24fbc8fbd07726e5928b2036dddd90cb73
Danijel Jovanović <danijel.xda@gmail.com>
2022-04-30 19:57:03 +0200

    Delete Namenski Računarski Sistemi/10 Arduino Simulator directory

--- Namenski Računarski Sistemi/10 Arduino Simulator/main.cpp
+++
@@ -1,8 +0,0 @@
-#include<arduinoSimulator.h>
-
-int main(){
-  // int startSimulator(int showGUI, int hideConsole, int startTasks, int startInterrupts, int startSerial);
-  startArduinoSimulator(true, true, true, true, true);
-  return 0;
-}
-
