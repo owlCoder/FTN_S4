a4a7ee340d3c15975667d4513068e2e416ecc6bc
Danijel Jovanović <danijel.xda@gmail.com>
2022-05-16 14:37:33 +0200

    v15 NRS

---
+++ Namenski Računarski Sistemi/15 Arduino Osnovni/15 Zadatak 01.ino
@@ -0,0 +1,17 @@
+void kopiranje(int id, void*tptr) 
+{
+   if (Serial.available())
+     Serial.write(Serial.read());
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  createTask(kopiranje, 50, TASK_ENABLE, NULL);
+}
+
+void loop()
+{
+
+}
+
